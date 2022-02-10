bb3b0f1d70db6cee7dc5a2799460bcb5dd60f5af
AnaMelisaGo <melisago.rockn@gmail.com>
2022-02-10 22:50:19 +0000

    Add function to view data from a worksheet

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import time
+from pprint import pprint
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -49,8 +50,8 @@ def validate_data(value):
             raise ValueError(
                 f'Use keywords: {test_keyword}, and comma with no spaces'
             )
-    except ValueError as e:
-        print(f'Invalid data: {e}')
+    except ValueError as error:
+        print(f'Invalid data: {error}')
         return False
     return True
 
@@ -88,28 +89,6 @@ def exit_app():
 
 
 # Based on Derek Shidler Tutorial for Adventure game
-def update_file():
-    """
-    Function that leads user according to choices:
-    - to update file
-    - to calculate data and close the worksheet to create a new one
-    - to save and exit the application
-    """
-    print('Choose an option:')
-    update = input('A-update file|B-tally & calc|C-exit app\n').upper()
-    if update == 'A':
-        add_new_data()
-    elif update == 'B':
-        tally_worksheet()
-    elif update == 'C':
-        exit_app()
-    else:
-        print('Choose A, B, or C only')
-        time.sleep(2)
-        update_file()
-
-
-# Based on Derek Shidler Tutorial for Adventure game
 def add_new_data():
     """
     Function to get data and update file
@@ -122,7 +101,7 @@ def add_new_data():
             patient_data = list(data)
             update_last_worksheet(patient_data)
         elif name == 'B':
-            print('You are back to update/calculate section\n')
+            print('You are back to update/tally/view section\n')
             time.sleep(2)
             update_file()
             break
@@ -137,7 +116,7 @@ def create_new_worksheet():
     To create a new worksheet
     """
     while True:
-        title = input('Type file name for the new worksheet:\n')
+        title = input('Type name for the new worksheet:\n')
         if validate_new_worksheet(title):
             print('Valid name!\n')
             break
@@ -151,6 +130,18 @@ def create_new_worksheet():
         }
     })
     print('New worksheet is created!\n')
+    time.sleep(1)
+
+
+def show_all_sheet():
+    """
+    To show all worksheet title
+    """
+    all_worksheets = CLINIC_SHEET.worksheets()
+    list_all_wsheet = []
+    for index in range(len(all_worksheets)):
+        list_all_wsheet.append(all_worksheets[index].title)
+    return list_all_wsheet
 
 
 def validate_new_worksheet(name):
@@ -158,36 +149,16 @@ def validate_new_worksheet(name):
     To validate new workwheet
     """
     try:
-        all_worksheets = CLINIC_SHEET.worksheets()
-        list_ws = []
-        for index, item in enumerate(all_worksheets):
-            list_ws.append(all_worksheets[index].title)
-        if name in list_ws:
+        all_worksheet = show_all_sheet()
+        if name in all_worksheet:
             raise ValueError(
                 'Oops! Use another title for the worksheet!\n'
             )
-    except ValueError as e:
-        print(f'Invalid name: {e}')
+    except ValueError as error:
+        print(f'Invalid name: {error}')
+        time.sleep(1)
         return False
     return True
-
-
-# Own code
-def check_last_worksheet():
-    """
-    Check if last worksheet is closed and calculated
-    """
-    last_worksheet = get_last_worksheet()
-    check_wksheet = last_worksheet.find('CLOSED')
-    if check_wksheet:
-        print('\nYou have to create a new worksheet')
-        print('Recommendation: use current "month-year" in naming file.\n')
-        create_new_worksheet()
-    else:
-        print('Found the last file...')
-        title = file_name()
-        print(f'FILE NAME: {title} \n')
-        time.sleep(1)
 
 
 def get_test_column():
@@ -209,7 +180,7 @@ def get_test_column():
     return total_test
 
 
-def file_name():
+def last_file_name():
     """
     Return file name of the last worksheet
     """
@@ -272,8 +243,8 @@ def add_data_revenue(file, rev):
     print(f'\nRevenue Report in {file}:')
     # From stackoverflow
     print('{:<12} {:>5}'.format('TEST', 'REVENUE'))
-    for k, v in rev_result.items():
-        print('{:<14} {:>5}'.format(k, v))
+    for key, val in rev_result.items():
+        print('{:<14} {:>5}'.format(key, val))
     time.sleep(2)
 
 
@@ -282,7 +253,7 @@ def calculate_total_revenue():
     To calculate and update the dr heart worksheets
     """
     test_list = get_test_column()
-    w_name = file_name()
+    w_name = last_file_name()
     data_list = row_data(w_name, test_list)
     update_total_tests(data_list)
     rev_data = get_revenue(data_list)
@@ -337,7 +308,7 @@ def tally_worksheet():
         calculate_total_revenue()
         continue_working()
     elif tally_input == 'N':
-        print('You still can update')
+        print('You can still update file')
         time.sleep(1)
         update_file()
     else:
@@ -346,6 +317,94 @@ def tally_worksheet():
         tally_worksheet()
 
 
+def show_file():
+    """
+    To show all data in a worksheet
+    """
+    print('\nGetting all files...')
+    list_worksheets = show_all_sheet()
+    for worksheet in list_worksheets:
+        print(worksheet)
+        time.sleep(0.5)
+    while True:
+        worksheet_inp = input('\nView file name:\n')
+        if worksheet_inp in list_worksheets:
+            show = CLINIC_SHEET.worksheet(worksheet_inp).get_all_values()
+            pprint(show)
+            break
+        if worksheet_inp not in list_worksheets:
+            print('\nFile name does nont exits!\n')
+            time.sleep(1)
+    view_file_option()
+
+
+def view_file_option():
+    """
+    To select show file, back or exit
+    """
+    print('\nPlease select an option')
+    view_inp = input('A-View file|B-Go back|C-Exit app\n').upper()
+    if view_inp == 'A':
+        show_file()
+        time.sleep(1)
+    elif view_inp == 'B':
+        print('You are back to update/tally/view section\n')
+        update_file()
+        time.sleep(1)
+    elif view_inp == 'C':
+        exit_app()
+    else:
+        print('Choose only A or B\n')
+        time.sleep(1)
+        view_file_option()
+
+
+# Own code
+def check_last_worksheet():
+    """
+    Check if last worksheet is closed and calculated
+    """
+    print('Checking files...')
+    time.sleep(1)
+    last_worksheet = get_last_worksheet()
+    check_wksheet = last_worksheet.find('CLOSED')
+    if check_wksheet:
+        print('\nYou have to create a new worksheet')
+        print('Recommendation: use current "month-year" in naming file.\n')
+        create_new_worksheet()
+    else:
+        print('Found the last file...')
+        time.sleep(1)
+        title = last_file_name()
+        print(f'FILE NAME: {title} \n')
+        time.sleep(1)
+
+
+# Based on Derek Shidler Tutorial for Adventure game
+def update_file():
+    """
+    Function that leads user according to choices:
+    - to update file
+    - to calculate data and close the worksheet to create a new one
+    - to save and exit the application
+    """
+    print('\nChoose an option:')
+    update = input('A-update file|B-tally |C-view |D-exit\n').upper()
+    if update == 'A':
+        add_new_data()
+    elif update == 'B':
+        tally_worksheet()
+    elif update == 'C':
+        view_file_option()
+    elif update == 'D':
+        exit_app()
+    else:
+        print('Choose A, B, or C only')
+        time.sleep(2)
+        update_file()
+
+
+# From Love Sandwich
 def main():
     """
     Main function
@@ -354,11 +413,13 @@ def main():
     update_file()
 
 
-print('-'*60)
-print('      Welcome to Dr. Heart Clinic')
-print('-'*60)
+print('-'*72)
+print('|{:^70}|'.format(''))
+print('|{:^70}|'.format('Welcome to Dr. Heart Clinic'))
+print('|{:^70}|'.format(''))
+print('-'*72)
 main()
-# calculate_total_revenue()
+
 # DONE-change close function to tally function, put reminder once tallied
 # DONE-file wont be accessible for update
 # DONE-deploy to HEROKU
@@ -369,3 +430,4 @@ main()
 # DONE - finished? add new function to choose whether add new ws or exit
 # DONE - add try exception when creating new files with SAME TITLE
 # DONE- exception APIError
+# make README
